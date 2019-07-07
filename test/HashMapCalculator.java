cc9ba93998fb3c39e5836b2b01e7d432d27371a3
League Student <league@WTS14-4.attlocal.net>
2019-07-06 17:20:14 -0700

    xdghfxbb

--- test/HashMapCalculator.java
+++ test/HashMapCalculator.java
@@ -5,43 +5,38 @@ public class HashMapCalculator {
 	public  Object commonKeyValuePairs(HashMap<String, String> hashmap1, HashMap<String, String> hashmap2) {
 		// TODO Auto-generated method stub
 int a = 0;
+		System.out.println("hi");
+
+
+for(String k: hashmap1.keySet()) {
+	System.out.println("hi");
+	if(hashmap2.containsKey(k)) {
+	
 		
-
-
+		
+		if(hashmap2.get(k)==hashmap1.get(k)) {
 			
-	if(hashmap1.containsKey("Alice")&&hashmap1.get("Alice")==hashmap2.get("Alice")) {
-		 a =2;
-	}
-	
-	if(hashmap1.containsKey("Mary")&&hashmap1.get("Mary")==hashmap2.get("Mary")) {
-		 a =2;
-	}
-	
-	if(hashmap1.containsKey("Bob")&&hashmap1.get("Bob")==hashmap2.get("Bob")) {
-		 a =2;
-	}
-	
-	if(hashmap1.containsKey("Chuck")&&hashmap1.get("Chuck")==hashmap2.get("Chuck")) {
-		 a =2;
-	}
-	
-	if(hashmap1.containsKey("Felix")&&hashmap1.get("Felix")==hashmap2.get("Felix")) {
-		 a =2;
-	}
+			
 		
-	if(hashmap1.containsKey("Ricardo")&&hashmap1.get("Ricardo")==hashmap2.get("Ricardo")) {
-		 a =2;
+a +=1;
+		
+			
 	}
+}
+
+
+
 	
-	if(hashmap1.containsKey("Tam")&&hashmap1.get("Tam")==hashmap2.get("Tam")) {
-		 a =2;
-	}
-	
-	
+		
+		
+		
 	
 
-		
-		return a;
-	}
+}
+System.out.println(a);
+return a;
 
+
+}
+	
 }
